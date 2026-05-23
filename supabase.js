5552f11a2e9d60ffe8c6f82d211c737eefb0c0a3
Paris Raja Ibelis <farisaziz690@gmail.com>
2026-05-23 15:41:28 +0700

    Menghapus file HTML lama yang tidak terpakai

--- supabase.js
+++
@@ -1,7 +0,0 @@
-const supabaseUrl = 'https://nwymuovgnaialfagcqgn.supabase.co';
-const supabaseKey = 'sb_publishable_eviwigCQXpkIPrh3wS7bZw_8EO6keCb';
-
-const supabaseClient = supabase.createClient(
-    supabaseUrl,
-    supabaseKey
-);
